1046d691a18d8f87aa113eb0d859347a992bccd9
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-05-28 10:24:48 +0900

    first commit

---
+++ src/test/java/jp/co/exbbs/ExBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.exbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
